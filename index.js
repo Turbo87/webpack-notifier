12ed1d0f01d80e781fdb8df59a50e9a8d616028e
Игорь Звягинцев <i.zvyagintsev@rambler-co.ru>
2019-12-10 19:15:31 +0300

    Some refactoring; multiple images for statuses
    (cherry picked from commit 4cc2c0bd164674ba9e8b4523606f3bf1797480e3)

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 var stripANSI = require('strip-ansi');
 var path = require('path');
-var objectAssign = require('object-assign');
 var os = require('os');
 var notifier = require('node-notifier');
 
@@ -13,22 +12,22 @@ function WebpackNotifierPlugin(options) {
 }
 module.exports = WebpackNotifierPlugin;
 
-WebpackNotifierPlugin.prototype.compileMessage = function (stats) {
-  function findFirstDFS(compilation, key) {
-    var match = compilation[key][0];
+function findFirstDFS(compilation, key) {
+  var match = compilation[key][0];
+  if (match) {
+    return match;
+  }
+
+  var children = compilation.children;
+  for (var i = 0; i < children.length; i += 1) {
+    match = findFirstDFS(children[i], key);
     if (match) {
       return match;
     }
-
-    var children = compilation.children;
-    for (var i = 0; i < children.length; i += 1) {
-      match = findFirstDFS(children[i], key);
-      if (match) {
-        return match;
-      }
-    }
   }
+}
 
+WebpackNotifierPlugin.prototype.compileEndOptions = function (stats) {
   if (this.isFirstBuild) {
     this.isFirstBuild = false;
 
@@ -37,17 +36,40 @@ WebpackNotifierPlugin.prototype.compileMessage = function (stats) {
     }
   }
 
+  var imageFromOptions = ('contentImage' in this.options)
+    ? this.options.contentImage
+    : DEFAULT_LOGO;
+
+  var successImage = '';
+  var warningsImage = '';
+  var errorsImage = '';
+  if (typeof imageFromOptions === 'object') {
+    successImage = imageFromOptions.success;
+    warningsImage = imageFromOptions.warning;
+    errorsImage = imageFromOptions.error;
+  }
+
   var hasEmoji = this.options.emoji;
   var error;
+  var contentImage;
+  var status;
   if (this.hasErrors(stats)) {
     error = findFirstDFS(stats.compilation, 'errors');
+    contentImage = errorsImage;
+    status = 'error';
   } else if (this.options.onlyOnError) {
     return;
   } else if (this.hasWarnings(stats) && !this.options.excludeWarnings) {
     error = findFirstDFS(stats.compilation, 'warnings');
+    contentImage = warningsImage;
+    status = 'warning';
   } else if (!this.lastBuildSucceeded || this.options.alwaysNotify) {
     this.lastBuildSucceeded = true;
-    return (hasEmoji ? '✅ ' : '') + 'Build successful';
+    return {
+      message: (hasEmoji ? '✅ ' : '') + 'Build successful',
+      contentImage: successImage,
+      status: 'success'
+    };
   } else {
     return;
   }
@@ -67,7 +89,11 @@ WebpackNotifierPlugin.prototype.compileMessage = function (stats) {
     message = (hasEmoji ? '⚠️ ' : '') + 'Warning: ' + message + error.message.toString();
   }
 
-  return stripANSI(message);
+  return {
+    message: stripANSI(message),
+    contentImage: contentImage,
+    status: status
+  };
 };
 
 WebpackNotifierPlugin.prototype.hasErrors = function (stats) {
@@ -81,22 +107,25 @@ WebpackNotifierPlugin.prototype.hasWarnings = function (stats) {
 };
 
 WebpackNotifierPlugin.prototype.compilationDone = function (stats) {
-  var msg = this.compileMessage(stats);
-  if (msg) {
-    var contentImage = ('contentImage' in this.options)
-      ? this.options.contentImage : DEFAULT_LOGO;
-
+  var { message, contentImage, status } = this.compileEndOptions(stats);
+  if (message) {
     var title = this.options.title;
     if (typeof title === 'function') {
-      title = title({ msg: msg });
+      title = title({ message: message, status: status });
     }
 
-    notifier.notify(objectAssign({
-      title: 'Webpack',
-      message: msg,
-      contentImage: contentImage,
-      icon: (os.platform() === 'win32' || os.platform() === 'linux') ? contentImage : undefined
-    }, this.options, { title: title }));
+    notifier.notify(Object.assign(
+      {
+        title: 'Webpack',
+        message: message,
+        contentImage: contentImage,
+        icon: (os.platform() === 'win32' || os.platform() === 'linux') ? contentImage : undefined
+      },
+      this.options,
+      {
+        title: title
+      }
+    ));
   }
 };
 
