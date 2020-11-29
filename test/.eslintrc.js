4a8cd4921d4df8e17a25f8ff5bd6977176920652
Gvozd <gvozdev.viktor@gmail.com>
2020-11-30 00:33:20 +0300

    ci(test): use jest for testing

---
+++ test/.eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    parserOptions: {
+        ecmaVersion: 2018
+    },
+    env: {
+        jest: true,
+    },
+};
---
+++ test/WebpackNotifierPlugin.test.js
@@ -0,0 +1,7 @@
+
+
+describe('WebpackNotifierPlugin', () => {
+    it('example', () => {
+
+    });
+});
