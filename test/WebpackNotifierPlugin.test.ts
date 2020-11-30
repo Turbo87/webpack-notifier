2b03d0cc1c6332096ee685b5d926164c4ed5926d
Gvozd <gvozdev.viktor@gmail.com>
2020-12-01 02:08:35 +0300

    ci(test): base tests for verbosity level configuration

--- test/WebpackNotifierPlugin.test.ts
+++ test/WebpackNotifierPlugin.test.ts
@@ -14,6 +14,34 @@ describe('WebpackNotifierPlugin', () => {
       [['warning'], {}],
     ])('%j %j', testChangesFlow);
   });
+  describe('verbosity level configuration', () => {
+    describe('Default', () => {
+      test.each([
+        [['successful', 'successful', 'successful'], {}],
+        [['error', 'error', 'successful'], {}],
+      ])('%j %j', testChangesFlow);
+    });
+    describe('Exclude Warnings', () => {
+      test.each([
+        [['warning'], {excludeWarnings: true}],
+      ])('%j %j', testChangesFlow);
+    });
+    describe('Always Notify', () => {
+      test.each([
+        [['successful', 'successful'], {alwaysNotify: true}],
+      ])('%j %j', testChangesFlow);
+    });
+    describe('Notify on error', () => {
+      test.each([
+        [['successful', 'warning', 'error'], {onlyOnError: true}],
+      ])('%j %j', testChangesFlow);
+    });
+    describe('Skip Notification on the First Build', () => {
+      test.each([
+        [['successful', 'successful'], {skipFirstNotification: true}],
+      ])('%j %j', testChangesFlow);
+    });
+  });
 });
 
 async function testChangesFlow(sources, opts)  {
