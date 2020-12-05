bfa438ead049c2a54b7af472340f57d06350cfa5
Gvozd <gvozdev.viktor@gmail.com>
2020-12-05 17:21:27 +0300

    ci(eslint): auto-fix fixable rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,17 @@
 module.exports = {
-    root: true,
-    env: {
-        'node': true
-    },
-    extends: [
-        'eslint:recommended',
-        'eslint-config-airbnb-base/legacy'
-    ],
-    rules: {
-        strict: 0,
-        indent: 0,
-        'quotes': 0,
-        'quote-props': 0,
-        'comma-dangle': 0,
-        'no-multi-assign': 0,
-        'space-before-function-paren': 0,
-        'no-plusplus': 0,
-        'curly': 0,
-        'nonblock-statement-body-position': 0,
-        'padded-blocks': 0,
-        'operator-linebreak': 0,
-        'semi': 0,
-        'object-curly-spacing': 0,
-        'vars-on-top': 0,
-        'consistent-return': 0,
-    }
+  root: true,
+  env: {
+    node: true
+  },
+  extends: [
+    'eslint:recommended',
+    'eslint-config-airbnb-base/legacy'
+  ],
+  rules: {
+    strict: 0,
+    'no-multi-assign': 0,
+    'no-plusplus': 0,
+    'vars-on-top': 0,
+    'consistent-return': 0
+  }
 };
--- index.js
+++ index.js
@@ -6,90 +6,90 @@ var notifier = require('node-notifier');
 
 var DEFAULT_LOGO = path.join(__dirname, 'logo.png');
 
-var WebpackNotifierPlugin = module.exports = function(options) {
-    this.options = options || {};
-    this.lastBuildSucceeded = false;
-    this.isFirstBuild = true;
+var WebpackNotifierPlugin = module.exports = function (options) {
+  this.options = options || {};
+  this.lastBuildSucceeded = false;
+  this.isFirstBuild = true;
 };
 
-WebpackNotifierPlugin.prototype.compileMessage = function(stats) {
-    function findFirstDFS(compilation, key) {
-        var match = compilation[key][0];
-        if (match) {
-            return match;
-        }
-
-        var children = compilation.children;
-        for (var i = 0; i < children.length; ++i) {
-            match = findFirstDFS(children[i], key);
-            if (match) {
-                return match;
-            }
-        }
+WebpackNotifierPlugin.prototype.compileMessage = function (stats) {
+  function findFirstDFS(compilation, key) {
+    var match = compilation[key][0];
+    if (match) {
+      return match;
     }
 
-    if (this.isFirstBuild) {
-        this.isFirstBuild = false;
-
-        if (this.options.skipFirstNotification) {
-            return;
-        }
+    var children = compilation.children;
+    for (var i = 0; i < children.length; ++i) {
+      match = findFirstDFS(children[i], key);
+      if (match) {
+        return match;
+      }
     }
+  }
 
-    var hasEmoji = this.options.emoji;
-    var error;
-    if (stats.hasErrors()) {
-        error = findFirstDFS(stats.compilation, 'errors');
-    } else if (this.options.onlyOnError) {
-        return;
-    } else if (stats.hasWarnings() && !this.options.excludeWarnings) {
-        error = findFirstDFS(stats.compilation, 'warnings');
-    } else if (!this.lastBuildSucceeded || this.options.alwaysNotify) {
-        this.lastBuildSucceeded = true;
-        return (hasEmoji ? '✅ ' : '') + 'Build successful';
+  if (this.isFirstBuild) {
+    this.isFirstBuild = false;
 
-    } else {
-        return;
+    if (this.options.skipFirstNotification) {
+      return;
     }
+  }
 
-    this.lastBuildSucceeded = false;
+  var hasEmoji = this.options.emoji;
+  var error;
+  if (stats.hasErrors()) {
+    error = findFirstDFS(stats.compilation, 'errors');
+  } else if (this.options.onlyOnError) {
+    return;
+  } else if (stats.hasWarnings() && !this.options.excludeWarnings) {
+    error = findFirstDFS(stats.compilation, 'warnings');
+  } else if (!this.lastBuildSucceeded || this.options.alwaysNotify) {
+    this.lastBuildSucceeded = true;
+    return (hasEmoji ? '✅ ' : '') + 'Build successful';
+  } else {
+    return;
+  }
 
-    var message = '';
-    if (error.module && error.module.rawRequest)
-        message = error.module.rawRequest + '\n';
+  this.lastBuildSucceeded = false;
 
-    if (error.error)
-        message = (hasEmoji ? '❌ ' : '') + 'Error: ' + message + error.error.toString();
-    else if (error.warning)
-        message = (hasEmoji ? '⚠️ ' : '') + 'Warning: ' + message + error.warning.toString();
-    else if (error.message) {
-        message = (hasEmoji ? '⚠️ ' : '') + 'Warning: ' + message + error.message.toString();
-    }
+  var message = '';
+  if (error.module && error.module.rawRequest) {
+    message = error.module.rawRequest + '\n';
+  }
 
-    return stripANSI(message);
+  if (error.error) {
+    message = (hasEmoji ? '❌ ' : '') + 'Error: ' + message + error.error.toString();
+  } else if (error.warning) {
+    message = (hasEmoji ? '⚠️ ' : '') + 'Warning: ' + message + error.warning.toString();
+  } else if (error.message) {
+    message = (hasEmoji ? '⚠️ ' : '') + 'Warning: ' + message + error.message.toString();
+  }
+
+  return stripANSI(message);
 };
 
-WebpackNotifierPlugin.prototype.compilationDone = function(stats) {
-    var msg = this.compileMessage(stats);
-    if (msg) {
-        var contentImage = ('contentImage' in this.options) ?
-            this.options.contentImage : DEFAULT_LOGO;
+WebpackNotifierPlugin.prototype.compilationDone = function (stats) {
+  var msg = this.compileMessage(stats);
+  if (msg) {
+    var contentImage = ('contentImage' in this.options)
+      ? this.options.contentImage : DEFAULT_LOGO;
 
-        var title = this.options.title
-        if (typeof title === 'function') {
-            title = title({msg: msg})
-        }
-
-        notifier.notify(objectAssign({
-            title: 'Webpack',
-            message: msg,
-            contentImage: contentImage,
-            icon: (os.platform() === 'win32' || os.platform() === 'linux') ? contentImage : undefined
-        }, this.options, { title: title }));
+    var title = this.options.title;
+    if (typeof title === 'function') {
+      title = title({ msg: msg });
     }
+
+    notifier.notify(objectAssign({
+      title: 'Webpack',
+      message: msg,
+      contentImage: contentImage,
+      icon: (os.platform() === 'win32' || os.platform() === 'linux') ? contentImage : undefined
+    }, this.options, { title: title }));
+  }
 };
 
-WebpackNotifierPlugin.prototype.apply = function(compiler) {
+WebpackNotifierPlugin.prototype.apply = function (compiler) {
   if (compiler.hooks) {
     var plugin = { name: 'Notifier' };
 
